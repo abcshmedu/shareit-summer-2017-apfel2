27488b82b7278cd9319587349f8a7e9667871e94
mountainflo <tobusch@hm.edu>
2017-04-26 15:42:14 +0200

    add Test-file

---
+++ Test/edu/hm/lipptobusch/shareit/businessLayer/MediaServiceImplTest.java
@@ -0,0 +1,23 @@
+/**
+ * Organisation: Hochschule Muenchen, Fakultaet 07 Informatik und Mathematik
+ * Purpose: lab software-architecture, IF4B, SS2017
+ * Purpose: solution of assignment 2
+ */
+package edu.hm.lipptobusch.shareit.businessLayer;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author Maximilian Lipp, lipp@hm.edu
+ * @author Florian Tobusch, tobusch@hm.edu
+ * @version 2017-04-19
+ */
+public class MediaServiceImplTest {
+    @Test
+    public void addBook() throws Exception {
+        assertEquals(true, true);
+    }
+
+}
