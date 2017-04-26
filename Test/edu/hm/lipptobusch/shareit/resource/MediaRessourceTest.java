45621ce533bc1ea60ebb66689999000b32e719a4
Max Lipp <mx5@mailbox.org>
2017-04-26 16:49:36 +0200

    1

--- Test/edu/hm/lipptobusch/shareit/resource/MediaRessourceTest.java
+++ Test/edu/hm/lipptobusch/shareit/resource/MediaRessourceTest.java
@@ -1,13 +1,22 @@
 package edu.hm.lipptobusch.shareit.resource;
 
+import edu.hm.lipptobusch.shareit.businessLayer.MediaServiceResult;
+import edu.hm.lipptobusch.shareit.models.Book;
 import org.junit.Test;
+
+import javax.ws.rs.core.Response;
+
+import static org.junit.Assert.*;
 
 /**
  * Created by mx on 26.04.17.
  */
 public class MediaRessourceTest {
     @Test
-    public void addBookTest(){
+    public void createBookTest(){
+        Response response = new MediaResource().createBook(new Book("title", "autor", "9783866801929"));
+
+        assertEquals(Response.status(200).entity(MediaServiceResult.OK).build(),response);
 
     }
 }
