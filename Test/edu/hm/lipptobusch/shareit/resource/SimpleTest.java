e0dad98080b22c51c54818bd692479b9bb29c453
Max Lipp <mx5@mailbox.org>
2017-04-28 03:59:32 +0200

    1

---
+++ Test/edu/hm/lipptobusch/shareit/resource/SimpleTest.java
@@ -0,0 +1,39 @@
+package edu.hm.lipptobusch.shareit.resource;
+
+import org.glassfish.jersey.server.ResourceConfig;
+import org.glassfish.jersey.test.JerseyTest;
+import org.junit.Test;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Application;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by mx on 28.04.17.
+ */
+public class SimpleTest extends JerseyTest {
+
+
+
+    @Path("hello")
+    public static class HelloResource {
+        @GET
+        public String getHello() {
+            return "Hello World!";
+        }
+    }
+
+
+    @Override
+    protected Application configure() {
+        return new ResourceConfig(HelloResource.class);
+    }
+
+    @Test
+    public void test() {
+        final String hello = target("hello").request().get(String.class);
+        assertEquals("Hello World!", hello);
+    }
+}
